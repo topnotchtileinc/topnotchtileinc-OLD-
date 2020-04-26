07ab7e398ad4cb8954b96b97161c3dbcaae659b1
topnotchtileinc <63125588+topnotchtileinc@users.noreply.github.com>
2020-04-25 17:55:44 -0700

    Update Gallery animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
+// MDB Lightbox Init
 $(function () {
-    var selectedClass = "";
-    $(".filter").click(function () {
-        selectedClass = $(this).attr("data-rel");
-        $("#gallery").fadeTo(100, 0.1);
-        $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
-        setTimeout(function () {
-            $("." + selectedClass).fadeIn().addClass('animation');
-            $("#gallery").fadeTo(300, 1);
-        }, 300);
-    });
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 });
